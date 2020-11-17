bf7abbaf02b122b767e8e87cadc2fe7832d0b8f0
Simon Sperr <simon.sperr@htl-wels.at>
2020-11-17 11:21:42 +0100

    Made the layout

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,13 @@
 package sample;
 
+import javafx.scene.control.Button;
+
 /**
  * @author Simon Sperr
  * @version 2020.1.1, 17.11.2020
  */
 public class Controller {
+
+    public Button convert_btn;
+
 }
