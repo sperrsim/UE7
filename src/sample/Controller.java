c289a1992f2d65e4404d6cba08ffd6da85db559b
Simon Sperr <simon.sperr@htl-wels.at>
2020-11-18 11:21:25 +0100

    Implemented the Initializable Interface on Controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,17 +1,25 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * @author Simon Sperr
  * @version 2020.1.1, 17.11.2020
  */
-public class Controller {
+public class Controller implements Initializable {
 
-    public Button convert_btn;
-    public TextField euro_txt;
-    public TextField yen_txt;
+
+    @FXML private Button convert_btn;
+    @FXML private TextField euro_txt;
+    @FXML private TextField yen_txt;
+
+    private CurrencyConverter converter;
 
     public void doConversion()
     {
@@ -24,7 +32,12 @@ public class Controller {
         {
             System.out.println("Fehlerhafter Wert");
         }
-        yen_txt.setText(String.valueOf(new CurrencyConverter().euroToYen(euro)));
+        yen_txt.setText(String.valueOf(this.converter.euroToYen(euro)));
 
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        converter = new CurrencyConverter();
     }
 }
